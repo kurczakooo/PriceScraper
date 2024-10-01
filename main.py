adf3b23a47f61dcf71d27d381298c66c2d7aa720
kurczakooo <121618421+kurczakooo@users.noreply.github.com>
2024-10-01 10:54:54 +0200

    its working, now to figure out the chart in real time

--- main.py
+++ main.py
@@ -4,32 +4,48 @@ import requests
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 import time
+from datetime import datetime
 import matplotlib.pyplot as plt
 import seaborn as sns
 from bs4 import BeautifulSoup
 
-PATH  = './chromedriver-win64/chromedriver.exe'
+price_df = pd.DataFrame(columns=['price', 'time'])
 
-samsung_monitor_morele_url = 'https://www.morele.net/monitor-samsung-viewfinity-s50gc-ls34c500gauxen-12767113/?utm_source=google&utm_medium=cpc&utm_campaign=20541966716&gad_source=1&gclid=Cj0KCQjwurS3BhCGARIsADdUH52flXIPpOT1haDmjaqYwsxML8CLmVd_urm1PDxjgiPR1H1GmQytKT0aAnkDEALw_wcB'
+while True:
+    PATH  = './chromedriver-win64/chromedriver.exe'
 
-service = Service(PATH)
+    samsung_monitor_morele_url = 'https://www.morele.net/monitor-samsung-viewfinity-s50gc-ls34c500gauxen-12767113/?utm_source=google&utm_medium=cpc&utm_campaign=20541966716&gad_source=1&gclid=Cj0KCQjwurS3BhCGARIsADdUH52flXIPpOT1haDmjaqYwsxML8CLmVd_urm1PDxjgiPR1H1GmQytKT0aAnkDEALw_wcB'
 
-driver = webdriver.Chrome(service = service)
-driver.get(samsung_monitor_morele_url)
+    service = Service(PATH)
 
-time.sleep(5)#to let the webiste load
+    driver = webdriver.Chrome(service = service)
+    driver.get(samsung_monitor_morele_url)
 
-html = driver.page_source
+    time.sleep(3)#to let the webiste load
 
-soup = BeautifulSoup(html, 'html.parser')
+    html = driver.page_source
 
-product_prices = soup.find_all('div', class_='product-price')
+    soup = BeautifulSoup(html, 'html.parser')
 
-for price in product_prices:
-        print(price.text.strip())
+    product_prices = soup.find_all('div', class_='product-price')
+
+    price = product_prices[0].text.strip()
+    formatted_price = float(price.replace('zł', '').replace(' ', '').replace(',', '.'))
+    t = datetime.now()
+    formatted_time = t.strftime("%d/%m/%Y %H:%M:%S")
+            
+    driver.quit()
+
+    print(f'cena: {formatted_price}, data: {formatted_time}')
+
+    with open('price_history.csv', 'a', encoding='utf-8') as file:
+        file.write(f'{i},{formatted_price},{formatted_time}\n')
+        file.close()
         
-driver.quit()
-
+    print('dodano wiersz do historii')
+    
+    time.sleep(3600)
+        
 
 """# Tworzenie pustego wykresu
 x, y = [], []
