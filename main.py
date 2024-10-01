9d3fe891ddab475577891f427d139de26ad4c7e8
kurczakooo <121618421+kurczakooo@users.noreply.github.com>
2024-10-01 11:04:00 +0200

    updated optimalization and the loop

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 import requests
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 import time
 from datetime import datetime
 import matplotlib.pyplot as plt
@@ -11,30 +12,33 @@ from bs4 import BeautifulSoup
 
 price_df = pd.DataFrame(columns=['price', 'time'])
 
+
+PATH  = './chromedriver-win64/chromedriver.exe'
+
+samsung_monitor_morele_url = 'https://www.morele.net/monitor-samsung-viewfinity-s50gc-ls34c500gauxen-12767113/?utm_source=google&utm_medium=cpc&utm_campaign=20541966716&gad_source=1&gclid=Cj0KCQjwurS3BhCGARIsADdUH52flXIPpOT1haDmjaqYwsxML8CLmVd_urm1PDxjgiPR1H1GmQytKT0aAnkDEALw_wcB'
+
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-gpu")
+service = Service(PATH)
+
+i = 1
 while True:
-    PATH  = './chromedriver-win64/chromedriver.exe'
-
-    samsung_monitor_morele_url = 'https://www.morele.net/monitor-samsung-viewfinity-s50gc-ls34c500gauxen-12767113/?utm_source=google&utm_medium=cpc&utm_campaign=20541966716&gad_source=1&gclid=Cj0KCQjwurS3BhCGARIsADdUH52flXIPpOT1haDmjaqYwsxML8CLmVd_urm1PDxjgiPR1H1GmQytKT0aAnkDEALw_wcB'
-
-    service = Service(PATH)
-
-    driver = webdriver.Chrome(service = service)
+    driver = webdriver.Chrome(service = service, options=chrome_options)
+    
     driver.get(samsung_monitor_morele_url)
-
     time.sleep(3)#to let the webiste load
 
     html = driver.page_source
-
     soup = BeautifulSoup(html, 'html.parser')
-
     product_prices = soup.find_all('div', class_='product-price')
+    driver.quit()
 
     price = product_prices[0].text.strip()
     formatted_price = float(price.replace('zł', '').replace(' ', '').replace(',', '.'))
     t = datetime.now()
     formatted_time = t.strftime("%d/%m/%Y %H:%M:%S")
-            
-    driver.quit()
 
     print(f'cena: {formatted_price}, data: {formatted_time}')
 
@@ -44,7 +48,8 @@ while True:
         
     print('dodano wiersz do historii')
     
-    time.sleep(3600)
+    time.sleep(10)
+    i += 1
         
 
 """# Tworzenie pustego wykresu
