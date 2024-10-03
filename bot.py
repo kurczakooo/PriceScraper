195459fbff31b9afeb592e8026f7e57f66ba1fc5
kurczakooo <121618421+kurczakooo@users.noreply.github.com>
2024-10-03 21:08:42 +0200

    trying to figure out the telegram bot

--- bot.py
+++ bot.py
@@ -2,22 +2,52 @@ from telegram import Bot
 from dotenv import load_dotenv
 import os
 import asyncio
+import telebot
 
 load_dotenv()
 key = os.getenv('API_KEY')
 
 
-chat_id ='xd'
+from telebot.async_telebot import AsyncTeleBot
+
+bot = AsyncTeleBot(key)
 
 
-bot = Bot(token=key)
+# Handle '/start' and '/help'
+@bot.message_handler(commands=['help', 'start'])
+async def send_welcome(message):
+    text = 'Hi, I am EchoBot.\nJust write me something and I will repeat it!'
+    await bot.reply_to(message, text)
 
-bot.send_message(chat_id=chat_id, text='siema')
+
+# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
+@bot.message_handler(func=lambda message: True)
+async def echo_message(message):
+    await bot.reply_to(message, message.text)
+
+bot.se
+
+asyncio.run(bot.polling())
+
+
+
+
+
+
+
+
+
+# chat_id = '@pkurczakooo'
+# bot = Bot(token=key)
+
+# async def notify(message):
+#     await bot.send_message(chat_id=chat_id, text=message)
 
 # async def get_chat_id():
 #     updates = await bot.get_updates()
+#     print("Updates received:", updates)
 #     for update in updates:
 #         print(update.message.chat_id)
         
-# if __name__ == '__main__':
-#     asyncio.run(get_chat_id())
+# if __name__ == '__bot__':
+#     asyncio.run(notify('xdddddd'))
