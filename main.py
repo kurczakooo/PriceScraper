0b63543b500f9bb0c3fd754ed88dc283991c4cbc
kurczakooo <121618421+kurczakooo@users.noreply.github.com>
2024-10-13 19:41:55 +0200

    changing the strategy

--- main.py
+++ main.py
@@ -18,38 +18,32 @@ PATH  = './chromedriver-win64/chromedriver.exe'
 samsung_monitor_morele_url = 'https://www.morele.net/monitor-samsung-viewfinity-s50gc-ls34c500gauxen-12767113/?utm_source=google&utm_medium=cpc&utm_campaign=20541966716&gad_source=1&gclid=Cj0KCQjwurS3BhCGARIsADdUH52flXIPpOT1haDmjaqYwsxML8CLmVd_urm1PDxjgiPR1H1GmQytKT0aAnkDEALw_wcB'
 
 chrome_options = Options()
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--headless")  # Tryb bezokienkowy
+
 service = Service(PATH)
 
 current_price = 0
 
-i = 1
-while i<2:
-    driver = webdriver.Chrome(service = service, options=chrome_options)
+driver = webdriver.Chrome(service = service, options=chrome_options)
+
+driver.get(samsung_monitor_morele_url)
+time.sleep(3)#to let the webiste load
+
+html = driver.page_source
+soup = BeautifulSoup(html, 'html.parser')
+product_prices = soup.find_all('div', class_='product-price')
+driver.quit()
+
+price = product_prices[0].text.strip()
+current_price = price
+formatted_price = float(price.replace('zł', '').replace(' ', '').replace(',', '.'))
+t = datetime.now()
+formatted_time = t.strftime("%d/%m/%Y %H:%M:%S")
+
+print(f'cena: {formatted_price}, data: {formatted_time}')
+
+with open('price_history.csv', 'a', encoding='utf-8') as file:
+    file.write(f'{2},{formatted_price},{formatted_time}\n')
+    file.close()
     
-    driver.get(samsung_monitor_morele_url)
-    time.sleep(3)#to let the webiste load
-
-    html = driver.page_source
-    soup = BeautifulSoup(html, 'html.parser')
-    product_prices = soup.find_all('div', class_='product-price')
-    driver.quit()
-
-    price = product_prices[0].text.strip()
-    current_price = price
-    formatted_price = float(price.replace('zł', '').replace(' ', '').replace(',', '.'))
-    t = datetime.now()
-    formatted_time = t.strftime("%d/%m/%Y %H:%M:%S")
-
-    print(f'cena: {formatted_price}, data: {formatted_time}')
-
-    with open('price_history.csv', 'a', encoding='utf-8') as file:
-        file.write(f'{i},{formatted_price},{formatted_time}\n')
-        file.close()
-        
-    print('dodano wiersz do historii')
-    
-    time.sleep(10)
-    i += 1
+print('dodano wiersz do historii')
