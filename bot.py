fdf719b8b8dd8e4abe299cc047fe28aca5686d6d
kurczakooo <121618421+kurczakooo@users.noreply.github.com>
2024-10-22 21:13:19 +0200

    made the bot send me th price after scraping

--- bot.py
+++ bot.py
@@ -1,63 +1,44 @@
 from telegram import Bot
 from dotenv import load_dotenv
 import os
-import asyncio
-import telebot
-from main import current_price
+from telegram.error import TelegramError
+#import asyncio
+#import telebot
+#from telebot.async_telebot import AsyncTeleBot
 
 load_dotenv()
 key = os.getenv('API_KEY')
 
+bot = Bot(key)
 
-from telebot.async_telebot import AsyncTeleBot
+async def notify_about_the_price(price):
+    try:
+        await bot.send_message(chat_id=903485794, text=f'{price} zł')
+        print("Message sent successfully.")
+    except TelegramError as e:
+        print(f"Failed to send message: {e}")
 
-bot = AsyncTeleBot(key)
 
+
+#THIS PART IS FOR HANDLING COMMANDS, 
+# MAYBE RUN ON ANOTHER THREAD OR STH, ANYWAYS IT'S RUNNING CONSTANTLY
 
 # Handle '/start' and '/help'
-@bot.message_handler(commands=['help', 'start'])
-async def send_welcome(message):
-    text = 'Hello, I will tell you the price of your product!'
-    await bot.reply_to(message, text)
+# @bot.message_handler(commands=['help', 'start'])
+# async def send_welcome(message):
+#     text = 'Hello, I will tell you the price of your product!'
+#     await bot.reply_to(message, text)
     
     
-@bot.message_handler(commands=['hej'])
-async def hihi(message):
-    text = 'Hej Liv'
-    await bot.reply_to(message, text)
+# @bot.message_handler(commands=['hej'])
+# async def hihi(message):
+#     text = 'Hej Liv'
+#     await bot.reply_to(message, text)
 
 
-# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
-@bot.message_handler(func=lambda message: True)
-async def echo_message(message):
-    await bot.reply_to(message, str(f'{current_price} zł'))
+# # Handle all other messages with content_type 'text' (content_types defaults to ['text'])
+# @bot.message_handler(func=lambda message: True)
+# async def echo_message(message):
+#     await bot.reply_to(message, str(f'{current_price} zł'))
 
-
-# async def notify_about_the_price(price):
-#     await bot.send_message(chat_id=903485794, text=str(f'{current_price} zł'))
-
-
-asyncio.run(bot.polling())
-
-
-
-
-
-
-
-
-
-# chat_id = '@pkurczakooo'
-# bot = Bot(token=key)
-
-# async def notify(message):
-#     await bot.send_message(chat_id=chat_id, text=message)
-
-# async def get_chat_id():
-#     updates = await bot.get_updates()
-#     print("Updates received:", updates)
-#     for update in updates:
-#         print(update.message.chat_id)
-        
-# if __name__ == '__bot__':
-#     asyncio.run(notify('xdddddd'))
+# asyncio.run(bot.polling())
