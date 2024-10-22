63e93fbf9b3a64f6121ea0100757c06188ec9453
kurczakooo <121618421+kurczakooo@users.noreply.github.com>
2024-10-22 21:14:05 +0200

    I needed to give up threading cause i have to use asynchronus functions

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 import threading
+import asyncio
 #import matplotlib.pyplot as plt
 #import seaborn as sns
 from urls import samsung_odyssey_g5_ceneo_url, xiaomi_g34wqi_ceneo_url
 from ceneo.scrape_price import scrape_ceneo_price
 from chromedriver_config.chromedriver_config import user_agent_string_override_command, chrome_options
 from data_management import DataManager
+from bot import notify_about_the_price
 
 
 #next I need to implement an algorithm to randomize the user string
@@ -19,7 +21,7 @@ samsung_prices_file = "data/samsung_g5_price_history.csv"
 xiaomi_prices_file = "data/xiaomi_g34wqi_price_history.csv"
 
 
-def main_fuction(SYSTEM_PATH, chromedriver_options, user_agent_string_override, url, data_file_path, url_type=None):
+async def main_fuction(SYSTEM_PATH, chromedriver_options, user_agent_string_override, url, data_file_path, url_type=None):
     service = Service(SYSTEM_PATH)
     driver = webdriver.Chrome(service = service, options=chromedriver_options)
     # Sends a Chrome DevTools Protocol (CDP) command to override the user-agent string
@@ -28,6 +30,8 @@ def main_fuction(SYSTEM_PATH, chromedriver_options, user_agent_string_override, 
     driver.get(url)
     prices_list = scrape_ceneo_price(driver=driver)
     current_price = prices_list[0]
+    
+    await notify_about_the_price(current_price)
 
     dataManager = DataManager(data_file_path)
     dataManager.load_data()
@@ -37,13 +41,17 @@ def main_fuction(SYSTEM_PATH, chromedriver_options, user_agent_string_override, 
 
 
 if __name__ == "__main__":
-    th1 = threading.Thread(
-        target=main_fuction, 
-        args=[PATH, chrome_options, user_agent_string_override_command, samsung_odyssey_g5_ceneo_url, samsung_prices_file])
+    # th1 = threading.Thread(
+    #     target=main_fuction, 
+    #     args=[PATH, chrome_options, user_agent_string_override_command, samsung_odyssey_g5_ceneo_url, samsung_prices_file]
+    # )
     
-    th2 = threading.Thread(
-        target=main_fuction, 
-        args=[PATH, chrome_options, user_agent_string_override_command, xiaomi_g34wqi_ceneo_url, xiaomi_prices_file])
+    # th2 = threading.Thread(
+    #     target=main_fuction, 
+    #     args=[PATH, chrome_options, user_agent_string_override_command, xiaomi_g34wqi_ceneo_url, xiaomi_prices_file]
+    # )
     
-    th1.start()
-    th2.start()
+    # th1.start()
+    # th2.start()
+    
+    asyncio.run(main_fuction(PATH, chrome_options, user_agent_string_override_command, xiaomi_g34wqi_ceneo_url, xiaomi_prices_file))
